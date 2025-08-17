0087a810c9132564468e164f42a7049fc82be761
Cidinei-Lima <cidinei.lima@hotmail.com>
2025-08-17 16:10:53 -0300

    :tada: Commit inicial

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
+const { withContentlayer } = require('next-contentlayer')
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withContentlayer(nextConfig);
